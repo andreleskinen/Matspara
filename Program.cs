ebb2eb7337a02208f7ba349b887d2a192f6c8a2a
Mona96-elb <143488384+Mona96-elb@users.noreply.github.com>
2024-08-23 09:55:32 +0200

    Min första commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World from Mona!");
         }
     }
 }
